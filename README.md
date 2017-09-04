c2e6519050ce1c8e7408e0cdbb0facda05b68af3
David Lejeune <dl@qbmt.be>
2017-09-04 15:11:58 +0200

    able to scrape dynamically from javascript

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 * pip install lxml
 * pip install pandas
 * pip install html5lib
+
+http://www.lfd.uci.edu/~gohlke/pythonlibs/#pyqt4
+download correct pyqt version : PyQwt‑5.2.1‑cp36‑cp36m‑win_amd64.whl
+
+pip install C:\users\David\Downloads\PyQt4-4.11.4-cp36-cp36m-win_amd64.whl
--- web-scraper.py
+++ web-scraper.py
@@ -1,6 +1,50 @@
+import sys
+from PyQt4.QtGui import QApplication
+from PyQt4.QtCore import QUrl
+from PyQt4.QtWebKit import QWebPage
 import bs4 as bs
 import urllib.request
 import pandas as pd
+
+class Client(QWebPage):
+
+    def __init__(self,url):
+        self.app = QApplication(sys.argv)
+        QWebPage.__init__(self)
+        self.loadFinished.connect(self.on_page_load)
+        self.mainFrame().load(QUrl(url))
+        self.app.exec_()
+
+
+    def on_page_load(self):
+        self.app.quit()
+
+url = 'https://pythonprogramming.net/parsememcparseface'
+client_response = Client(url)
+source = client_response.mainFrame().toHtml()
+
+
+# scrape dynamically changing content of website
+# sauce = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface')
+soup = bs.BeautifulSoup(source, 'lxml')
+js_test = soup.find('p' , class_='jstest')
+print(js_test.text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # sauce = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
 # soup = bs.BeautifulSoup(sauce, 'lxml')
@@ -99,9 +143,3 @@ print(sauce) # sauce will add a ton of tabs and newlines (not clean)
 # print("-----------------------------------")
 # for url in soup.find_all('loc'):
 #     print(url.text)
-
-# scrape dynamically changing content of website
-sauce = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface')
-soup = bs.BeautifulSoup(sauce, 'lxml')
-js_test = soup.find('p' , class_='jstest')
-print(js_test.text)
