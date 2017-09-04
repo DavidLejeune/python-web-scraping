2a7beefd0eb803a8b9e3f00e63fad253ed1366ca
David Lejeune <dl@qbmt.be>
2017-09-04 11:27:19 +0200

    testing html element title

--- web-scraper.py
+++ web-scraper.py
@@ -2,7 +2,14 @@ import bs4 as bs
 import urllib.request
 
 sauce = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
-
 soup = bs.BeautifulSoup(sauce, 'lxml')
 
-print(soup)
+'''
+print(soup)  # soup will give a nicely arranged html (clean)
+print(sauce) # sauce will add a ton of tabs and newlines (not clean)
+'''
+
+print("Testing html element TITLE")
+print(soup.title)
+print(soup.title.name)
+print(soup.title.string) # = print(soup.title.text)
