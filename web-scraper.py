728eabdc623e8ad13343c132b944560bd83c53a5
David Lejeune <dl@qbmt.be>
2017-09-04 11:58:22 +0200

    printing all the nav's

--- web-scraper.py
+++ web-scraper.py
@@ -9,38 +9,44 @@ print(soup)  # soup will give a nicely arranged html (clean)
 print(sauce) # sauce will add a ton of tabs and newlines (not clean)
 '''
 
-print("Testing html element TITLE")
-print("--------------------------")
-print(soup.title)
-print(soup.title.name)
-print(soup.title.string) # = print(soup.title.text)
-print("")
+# print("Testing html element TITLE")
+# print("--------------------------")
+# print(soup.title)
+# print(soup.title.name)
+# print(soup.title.string) # = print(soup.title.text)
+# print("")
+#
+#
+# print("Print first paragraph element")
+# print("-----------------------------")
+# print(soup.p)
+# print("")
+#
+# print("Print all paragraph elements")
+# print("----------------------------")
+# print(soup.find_all('p'))
+# print("")
+#
+#
+# print("Print all paragraph elements with FOR loop")
+# print("------------------------------------------")
+# for paragraph in soup.find_all('p'):
+#     print(paragraph.text)
+# print("")
+#
+# print("Getting all the text")
+# print("--------------------")
+# print(soup.get_text())
+# print("")
+#
+# print("Finding all the links")
+# print("---------------------")
+# for url in soup.find_all('a'):
+#     print(url.get('href'))
+# print("")
 
-
-print("Print first paragraph element")
-print("-----------------------------")
-print(soup.p)
-print("")
-
-print("Print all paragraph elements")
-print("----------------------------")
-print(soup.find_all('p'))
-print("")
-
-
-print("Print all paragraph elements with FOR loop")
-print("------------------------------------------")
-for paragraph in soup.find_all('p'):
-    print(paragraph.text)
-print("")
-
-print("Getting all the text")
-print("--------------------")
-print(soup.get_text())
-print("")
-
-print("Finding all the links")
+print("Finding all the nav's")
 print("---------------------")
-for url in soup.find_all('a'):
-    print(url.get('href'))
+nav = soup.nav
+print(nav)
 print("")
